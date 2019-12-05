7ed12c7d5365bb87362199cd279f64a479e50ce8
nimestel <cherv.tatiana@gmail.com>
2019-12-05 22:33:40 +0300

    Тест на удаление первой группы
    Общая фикстура вынесена в отдельный конфиг
    Запуск браузера один раз для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -26,3 +26,10 @@ class GroupHelper:
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_css_selector(".msgbox a[href='group.php']").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
---
+++ test/test_del_group.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.group import Group
+from fixture.application import Application
+
+
+def test_delete_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.delete_first_group()
+    app.session.logout()
