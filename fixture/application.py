1ed9a3423234dec10bb39f1236cc93e183750e4c
nimestel <cherv.tatiana@gmail.com>
2019-12-05 22:12:20 +0300

    Выделен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,19 +1,20 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
-
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(5)
+        self.session = SessionHelper(self)
 
-    def logout(self):
+    def open_home_page(self):
         wd = self.wd
-        wd.find_element_by_link_text("Выйти").click()
+        wd.get("http://addressbook/")
 
-    def return_to_groups_page(self):
+    def open_groups_page(self):
         wd = self.wd
-        wd.find_element_by_css_selector(".msgbox a[href='group.php']").click()
+        wd.find_element_by_css_selector("a[href='group.php']").click()
 
     def create_group(self, group):
         wd = self.wd
@@ -31,24 +32,9 @@ class Application:
         wd.find_element_by_css_selector("input[type='submit']").click()
         self.return_to_groups_page()
 
-    def open_groups_page(self):
+    def return_to_groups_page(self):
         wd = self.wd
-        wd.find_element_by_css_selector("a[href='group.php']").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type='submit']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://addressbook/")
+        wd.find_element_by_css_selector(".msgbox a[href='group.php']").click()
 
     def destroy(self):
         self.wd.quit()
