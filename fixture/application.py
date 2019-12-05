57a12f3bc382881ba8fde2157345e5f0b865a7f9
nimestel <cherv.tatiana@gmail.com>
2019-12-05 22:18:16 +0300

    Выделен класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
 
 class Application:
 
@@ -7,34 +9,11 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://addressbook/")
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_css_selector("a[href='group.php']").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_css_selector("input[type='submit']").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_css_selector(".msgbox a[href='group.php']").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="qwerty", header="qwerty", footer="qwerty"))
+    app.group.create(Group(name="qwerty", header="qwerty", footer="qwerty"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
