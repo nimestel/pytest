f770a6366e89639bbcb3c4f9fa00ac7118f06752
nimestel <cherv.tatiana@gmail.com>
2020-01-07 22:58:45 +0300

    Загрузка тестовых данных из json

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import importlib
 import json
 import pytest
 import os.path
+import jsonpickle
 from fixture.application import Application
 
 fixture = None
@@ -43,7 +44,15 @@ def pytest_generate_tests(metafunc):
         if fixture.startswith("data_"):
             testdata = load_from_module(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        if fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
 
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
