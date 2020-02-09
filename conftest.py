d23fe93c59419a72705abac3694f0699cf14b487
nimestel <cherv.tatiana@gmail.com>
2020-02-09 19:37:12 +0300

    Сравнение списка объектов в БД и интерфейсе

--- conftest.py
+++ conftest.py
@@ -22,9 +22,8 @@ def load_config(file):
 @pytest.fixture
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target")['web'])
+    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
     fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
@@ -33,11 +32,13 @@ def app(request):
 
 @pytest.fixture(scope="session")
 def db(request):
-    db_config = load_config(request.config.getoption("--target")['db'])
+    db_config = load_config(request.config.getoption("--target"))['db']
     dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'],
                           password=db_config['password'])
+
     def fin():
         dbfixture.destroy()
+
     request.addfinalizer(fin)
     return dbfixture
 
--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+from model.group import Group
+
 
 class DbFixture:
     def __init__(self, host, name, user, password):
@@ -8,6 +10,18 @@ class DbFixture:
         self.user = user,
         self.password = password
         self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        group_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT group_id, group_name, group_header, group_footer FROM group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return group_list
 
     def destroy(self):
         self.connection.close()
