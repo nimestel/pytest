fb3a647e827b883694165ea6ea586eb72dcd17c7
nimestel <cherv.tatiana@gmail.com>
2020-02-09 20:04:04 +0300

    Оптимизация тестов с помощью запросов к БД

--- fixture/group.py
+++ fixture/group.py
@@ -48,6 +48,15 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+
     def modify_first_group(self, new_group_data):
         self.modify_group_by_index(0, new_group_data)
 
@@ -67,6 +76,11 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
 
     def count(self):
         wd = self.app.wd
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,12 +1,10 @@
 from model.group import Group
-
+from timeit import timeit
 
 def test_group_list(app, db):
-    ui_list = app.group.get_group_list()
-
+    print(timeit(lambda: app.group.get_group_list(), number=1))
     def clean(group):
-        return Group(id=group.id, name=group.name.strip())
+         return Group(id=group.id, name=group.name.strip())
+    print(timeit(lambda: map(clean, db.get_group_list()), number=1000))
 
-    db_list = map(clean, db.get_group_list())
-
-    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    #assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
